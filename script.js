284710dbf43d51b99e4eab33083fb2c49abe1d1c
Maria Recskova <maria.recska@gmail.com>
2020-06-08 12:17:10 +0200

    app ready

---
+++ script.js
@@ -0,0 +1,25 @@
+const container = document.getElementById("container");
+const text = document.getElementById("text");
+
+const totalTime = 9000;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breatheAnimation();
+
+function breatheAnimation() {
+    text.innerHTML = "Breath in!";
+    container.className = "container-grow";
+
+    setTimeout(() => {
+        text.innerText = "Hold";
+
+        setTimeout(() => {
+            text.innerText = "Breathe Out!";
+            container.className = "container-shrink";
+        }, holdTime)
+
+    }, breatheTime)
+}
+
+setInterval(breatheAnimation, totalTime);
